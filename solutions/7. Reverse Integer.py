305b84f78452dbee9576b1522e0434e0f638f415
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-01-12 18:00:04 +0530

    Add

--- solutions/7. Reverse Integer.py
+++ solutions/7. Reverse Integer.py
@@ -1,3 +1,17 @@
 class Solution:
-    def reverse(self, x: int) -> int:
-        
+    def check_constraint(self,result):
+        return -2**31<=result<=2**31-1
+    
+    def reverse(self, x: int) -> int:
+        if self.check_constraint(x):
+            if x>0:
+                x=str(x)    
+                result = int(x[::-1])
+            elif x<0:
+                x=str(x)
+                result = -int(x[:0:-1])
+            else:
+                return 0
+            return result if self.check_constraint(result) else 0
+        else:
+            return 0
