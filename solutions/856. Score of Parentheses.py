fbf9ed80d2f579f8cf23ffb4cf1c190d83876a4c
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-01-24 23:11:08 +0530

    24 ms

---
+++ solutions/856. Score of Parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def scoreOfParentheses(self, S: str) -> int:
+        stack =[]
+        for i in S:
+            if i=='(':
+                stack.append(i)
+            else:
+                top = stack.pop()
+                if top=='(':
+                    stack.append(1)
+                else:
+                    result=0
+                    while top!="(":
+                        result+=top
+                        if not stack:
+                            break
+                        top = stack.pop()
+                    stack.append(2*result)
+        return sum(stack)
+        
