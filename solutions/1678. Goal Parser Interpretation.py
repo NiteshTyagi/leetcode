27daaf4ff8d7a69e508e6ea11eca1036ba3b6ead
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-02-03 17:12:12 +0530

    [ALT] Alternative Solution

--- solutions/1678. Goal Parser Interpretation.py
+++ solutions/1678. Goal Parser Interpretation.py
@@ -1,4 +1,21 @@
 class Solution:
     def interpret(self, command: str) -> str:
         return "o".join(command.split('()')).replace('(','').replace(')','')
+        # stack = []
+        # for i in command:
+        #     if i==')':
+        #         element = stack.pop()
+        #         s_temp=''
+        #         while element!='(':
+        #             s_temp+=element
+        #             if not stack:
+        #                 break
+        #             element=stack.pop()
+        #         if s_temp:
+        #             stack.append(s_temp[::-1])
+        #         else:
+        #             stack.append('o')
+        #     else:
+        #         stack.append(i)
+        # return "".join(stack)
         
