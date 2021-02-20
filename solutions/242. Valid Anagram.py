c4c6a82d8c1c3992576f616345f73fe83719e105
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-02-20 12:58:28 +0530

    44 ms

--- solutions/242. Valid Anagram.py
+++ solutions/242. Valid Anagram.py
@@ -1,5 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = sorted(s)
-        t = sorted(t)
-        return True if s==t else False
+        d = dict()
+        if len(s)!=len(t):
+            return False
+        for i in 'abcdefghijklmnopqrstuvwxyz':
+            d[i]=0
+            
+        for i in range(len(s)):
+            d[s[i]]+=1
+            d[t[i]]-=1
+            
+        return all(i==0 for i in d.values())
