8d3be926c55c3447f5529b681de016087123284d
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-03-01 02:04:34 +0530

    20 ms

--- solutions/1342. Number of Steps to Reduce a Number to Zero.py
+++ solutions/1342. Number of Steps to Reduce a Number to Zero.py
@@ -1,14 +1,13 @@
 class Solution:
     def numberOfSteps (self, num: int) -> int:
-        if num<1:return 0
-        result=-1
+        
+        result=0
         while num:
-            # print(num,result)
             result+=1
             if num&1:
-                result+=1
                 num-=1
-            num = num//2
+            else:
+                num = num>>1
                           
         return result
         
