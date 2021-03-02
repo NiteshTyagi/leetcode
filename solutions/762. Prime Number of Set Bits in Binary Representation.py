9b979ed4c92ab007ab402fe77723cc8c8b3c85df
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-03-03 04:52:34 +0530

    608 ms

--- solutions/762. Prime Number of Set Bits in Binary Representation.py
+++ solutions/762. Prime Number of Set Bits in Binary Representation.py
@@ -12,18 +12,11 @@ class Solution:
             countB = countBit(i)
             if countB:
                 setBit.append(countB)
-        # print(setBit)
         
         countP=0
-        def checkPrime(n):
-            cp=0
-            for i in range(1,n+1):
-                if n%i==0:
-                    cp+=1
-                    
-            return True if cp==2 else False
+        PrimeNumbers = [2,3,5,7,11,13,17,19]
                 
         for i in setBit:
-            if checkPrime(i):
+            if i in PrimeNumbers:
                 countP+=1
         return (countP)
