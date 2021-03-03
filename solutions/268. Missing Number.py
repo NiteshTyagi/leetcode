e1227b2ada3bbb1aef8298182690d8ce178c9032
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-03-03 07:59:20 +0530

    128 ms

--- solutions/268. Missing Number.py
+++ solutions/268. Missing Number.py
@@ -1,6 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         size = len(nums)
-        for i in range(size+1):
-            if i not in nums:
-                return i
+        actualSum  = sum(range(size+1))
+        expectedSum = sum(nums)
+        return actualSum - expectedSum
