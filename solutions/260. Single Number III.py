ff90d6e02b54cb34ef40776fe4bb4edd8d52dafe
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-03-07 14:18:31 +0530

    48 ms

--- solutions/260. Single Number III.py
+++ solutions/260. Single Number III.py
@@ -1,5 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        count = list(filter(lambda x: nums.count(x)==1,nums))
-        return count
+        from collections import Counter
+        count = Counter(nums)
+        return list(map(lambda x:x[0],filter(lambda x: x[1]==1,count.items())))
+        
         
