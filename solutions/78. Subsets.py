a79e3b275d1fb32c7a3d04caefd5c9a75ca1af06
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-03-07 23:22:00 +0530

    32 ms

--- solutions/78. Subsets.py
+++ solutions/78. Subsets.py
@@ -1,11 +1,6 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        n = len(nums)
-        nth_bits = 1<<n
-        for i in range(2**n):
-            bitmask = bin(i|nth_bits)[3:]
-            
-            result.append([nums[j] for j in range(n) if bitmask[j]=='1'])
-​
+        result = [[]]
+        for i in nums:
+            result += [j+[i] for j in result]
         return result
