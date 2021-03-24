662e252e20e9c1c6627e6bc3112eee7a970b1677
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-03-24 20:20:44 +0530

    720 ms

--- solutions/997. Find the Town Judge.py
+++ solutions/997. Find the Town Judge.py
@@ -1,11 +1,11 @@
 class Solution:
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
-        d = {i:[0,0] for i in range(1,N+1)}
-        # print(d)
+        d = [0]*(N+1)
         for a,b in trust:
-            d[b][0]+=1
-            d[a][1]+=1
-        # print(d)
-        f=list(filter(lambda x:True if x[1][0]==N-1 and x[1][1]==0 else False,d.items()))
-        return f[0][0] if len(f)==1 else -1
-        
+            d[a]-=1
+            d[b]+=1
+            
+        for i in range(1,N+1):
+            if d[i]==N-1:
+                return i
+        return -1
