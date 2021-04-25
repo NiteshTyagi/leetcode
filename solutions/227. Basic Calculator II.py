345e29409ea23ef2007c2a831a6c50fc35fea8cc
Pythonista@nitesh <33661780+NiteshTyagi@users.noreply.github.com>
2021-04-25 13:56:39 +0530

    196 ms

--- solutions/227. Basic Calculator II.py
+++ solutions/227. Basic Calculator II.py
@@ -9,6 +9,7 @@ class Solution:
                 return 0
             else:
                 return 1 
+            
         def apply_operator(op1,op2,op):
             if op=='+':return op1+op2
             elif op=='-':return op1-op2
@@ -19,33 +20,33 @@ class Solution:
             if s[i]==' ':
                 i+=1
                 continue
+                
             elif s[i].isdigit():
-                temp = s[i]
-                i+=1
+                temp = ''
                 while i<len(s) and s[i].isdigit():
                     temp+=s[i]
                     i+=1
                 operand.append(int(temp))
+                i-=1
+                
             elif s[i] in ['+','-','/','*']:
                 currentop = s[i]
                 if s[i]=='/':
                     currentop = '//'
+                while (len(operator)!=0 and check_prec(operator[-1]) >= check_prec(currentop)):
+                    op1 = operand.pop()
+                    op2 = operand.pop()
+                    op = operator.pop()
+                    result= apply_operator(op2,op1,op)
+                    operand.append(result)
                 operator.append(currentop)
-            #     if not operator: 
-            #         operator.append(currentop)
-            #     elif check_prec(currentop) > check_prec(operator[-1]):
-            #         operator.append(currentop)
-            #     else:
-            #         op1 = operand.pop()
-            #         op2 = operand.pop()
-            #         op = operator.pop()
-            #         result= apply_operator(op2,op1,op)
-            #         operand.append(result)
-            #         operator.append(currentop)
-                i+=1
-            # print(operand,operator,sep='<-operand--1-1-1-1---operator-->')      
-        temp=''
-        while operand and operator:
-            temp+=str(operand.pop(0))+operator.pop(0)
-        temp+=str(operand.pop())
-        return eval(temp)
+            i+=1
+            
+        while len(operator) != 0:
+            op1 = operand.pop()
+            op2 = operand.pop()
+            op = operator.pop()
+            result= apply_operator(op2,op1,op)
+            operand.append(result)
+        return operand[-1]
+        
